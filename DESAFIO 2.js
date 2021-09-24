bf37dd77e18ae1bcb31cc9580bf37c9faa63293d
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-09-24 17:39:56 -0300

    Add files via upload

---
+++ DESAFIO 2.js
@@ -0,0 +1,57 @@
+/*
+Neste desfio , a tarefa é de implementar uma função tripleChances que:
+
+-receba um argumento
+ chances uma array de
+ inteiros;
+
+-retorna uma array de 
+inteiros onde cada elemento
+desse novo array seja o
+elemento do inicial
+multiplicado por 3;
+
+Sua implementação da função
+será testada por um código
+fornecido com uma série de arquivos
+de entrada.Cada arquivo de entrada 
+contem parametros para a chamada da função.
+A função será chamada com esses parametros e o
+resultado da execução será impressa na saída.
+
+-A primeira linha contem 
+um inteiro n , que denota o
+número de elementos em chances
+Cada linha i das n
+subsequentes linhas (onde
+0 < i < n) contem um inteiro
+descrevendo chances[i] onde 
+i > 0 e i < n
+
+AMOSTRA DE ENTRADA 
+5
+2
+3
+5
+8
+10
+AMOSTRA DE SAÍDA
+6
+9
+15
+24
+30
+*/
+
+
+/*
+ * Complete a função 'tripleTheChances' abaixo.
+ *
+ * O retorno da função é uma variável do tipo INTEGER_ARRAY.
+ * A função aceita o parâmetro chances do tipo INTEGER_ARRAY.
+ */
+
+function tripleTheChances(chances) {
+    // Write your code here
+
+function main() {
